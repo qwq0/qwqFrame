82fd9d6a7f7cdb5b1ae26f7f5b57b4f7a87d4110
qwq0 <82009862+qwq0@users.noreply.github.com>
2023-11-20 17:51:09 +0800

    ud

--- src/dom/tool/event/touch.js
+++ src/dom/tool/event/touch.js
@@ -21,9 +21,14 @@ export function touchBind(element, callBack)
         capture: false,
         passive: true
     });
+    element.addEventListener("touchcancel", e => touchCancel(/** @type {TouchEvent} */(e)), {
+        capture: false,
+        passive: true
+    });
 
     /**
-     * @type {Array<{
+     * 触摸点id 到 触摸点信息 映射
+     * @type {Map<number, {
      *  id: number,
      *  sx: number,
      *  sy: number,
@@ -31,22 +36,8 @@ export function touchBind(element, callBack)
      *  y: number
      * }>}
      */
-    let ogTouches = [];
-    /**
-     * 通过标识符取触摸点数据索引
-     * @param {any} id
-     * @returns {number}
-     */
-    function getTouchesInd(id)
-    {
-        let ret = -1;
-        ogTouches.forEach((o, i) =>
-        {
-            if (id == o.id)
-                ret = i;
-        });
-        return ret;
-    }
+    let touchesSet = new Map();
+
     /**
      * 触摸处理函数(按下)
      * @param {TouchEvent} e 
@@ -64,7 +55,7 @@ export function touchBind(element, callBack)
                 x: o.clientX,
                 y: o.clientY
             };
-            ogTouches.push(t);
+            touchesSet.set(o.identifier, t);
             callBack(new PointerData(
                 t.x, t.y,
                 0, 0,
@@ -73,6 +64,7 @@ export function touchBind(element, callBack)
             ));
         });
     }
+
     /**
      * 触摸处理函数(移动)
      * @param {TouchEvent} e 
@@ -81,45 +73,65 @@ export function touchBind(element, callBack)
     {
         forEach(e.changedTouches, o =>
         {
-            let ind = getTouchesInd(o.identifier);
-            if (ind > -1)
+            let touchInfo = touchesSet.get(o.identifier);
+            if (touchInfo)
             {
-                let t = ogTouches[ind];
-                let vx = o.clientX - t.x;
-                let vy = o.clientY - t.y;
-                t.x = o.clientX;
-                t.y = o.clientY;
+                let vx = o.clientX - touchInfo.x;
+                let vy = o.clientY - touchInfo.y;
+                touchInfo.x = o.clientX;
+                touchInfo.y = o.clientY;
                 callBack(new PointerData(
-                    t.x, t.y,
+                    touchInfo.x, touchInfo.y,
                     vx, vy,
-                    t.sx, t.sy,
+                    touchInfo.sx, touchInfo.sy,
                     true, false
                 ));
             }
         });
     }
+
     /**
      * 触摸处理函数(松开)
      * @param {TouchEvent} e 
      */
     function touchEnd(e)
     {
-        e.changedTouches
         forEach(e.changedTouches, o =>
         {
-            let ind = getTouchesInd(o.identifier);
-            if (ind > -1)
+            let touchInfo = touchesSet.get(o.identifier);
+            if (touchInfo)
             {
-                let t = ogTouches[ind];
-                ogTouches.splice(ind, 1);
-                let vx = o.clientX - t.x;
-                let vy = o.clientY - t.y;
-                t.x = o.clientX;
-                t.y = o.clientY;
+                touchesSet.delete(o.identifier);
+                let vx = o.clientX - touchInfo.x;
+                let vy = o.clientY - touchInfo.y;
+                touchInfo.x = o.clientX;
+                touchInfo.y = o.clientY;
                 callBack(new PointerData(
-                    t.x, t.y,
+                    touchInfo.x, touchInfo.y,
                     vx, vy,
-                    t.sx, t.sy,
+                    touchInfo.sx, touchInfo.sy,
+                    false, false
+                ));
+            }
+        });
+    }
+
+    /**
+     * 触摸处理函数(触摸取消)
+     * @param {TouchEvent} e 
+     */
+    function touchCancel(e)
+    {
+        forEach(e.changedTouches, o =>
+        {
+            let touchInfo = touchesSet.get(o.identifier);
+            if (touchInfo)
+            {
+                touchesSet.delete(o.identifier);
+                callBack(new PointerData(
+                    touchInfo.x, touchInfo.y,
+                    0, 0,
+                    touchInfo.sx, touchInfo.sy,
                     false, false
                 ));
             }
